5bc46fd05d89b9aabc6f5f6124a36b42939da78a
David Kaufmann <david@kaufmann.dev>
2023-04-10 08:22:09 +0200

    Update 5a_abschlusstest.mongodb.js

--- c_insy/dql/5a_abschlusstest.mongodb.js
+++ c_insy/dql/5a_abschlusstest.mongodb.js
@@ -1,3 +1,46 @@
 // Gib mir alle Sections, die zu einer terminierenden Seciton führen.
+// Terminierende Sections haben als EventType "MISSION_FAILED" oder "MISSION_ACCOMPLISHED_EVENT"
 
-// terminierende Sections haben als EventType "MISSION_FAILED" oder "MISSION_ACCOMPLISHED_EVENT"
+db.lw1.find({});
+db.lw1.aggregate([
+    {
+        $match: {
+            "events" : {
+                $elemMatch : {
+                    eventType : {
+                        $in : ["MISSION_FAILED_EVENT", "MISSION_ACCOMPLISHED_EVENT"]
+                    }
+                }
+            }
+        }
+    }
+])
+
+// die region ausgeben wo die meisten kämpfe stattgefunden haben
+db.lw1.aggregate([
+    {
+        $unwind : "$events"
+    },
+    {
+        $group: {
+          _id: "$_id",
+          eventCount: {
+            $sum : {
+                $cond : [
+                    { $eq: [ "$events.eventType", "COMBAT" ] },
+                    1,
+                    0
+                ]
+            }
+          }
+        }
+    },
+    {
+      $sort: {
+        eventCount: -1
+      }
+    },
+    {
+      $limit: 1
+    }
+])
