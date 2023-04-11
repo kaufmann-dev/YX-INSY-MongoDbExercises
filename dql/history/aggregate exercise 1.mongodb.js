52966e1abaee8dc9d392bb148a82de15ea49f183
David Kaufmann <david@kaufmann.dev>
2023-04-11 11:33:05 +0200

    comment use statements

---
+++ dql/history/aggregate exercise 1.mongodb.js
@@ -0,0 +1,183 @@
+use ('insy');
+ // -- --------------------------------------------------------------------------------- --
+// --  1.Beispiel) Aggregationsframework: $match, $project, $sort, $out (1.Punkt)
+// -- --------------------------------------------------------------------------------- --
+// Sammeln Sie die folgenden Eventdaten in der "eventsReport" Collection. Berücksichtigen
+// Sie nur Battles. Für das Event muß zumindestens ein Review abgegeben worden sein.
+
+
+// Ausgabe: eventName, battleVictor, description, maxReview
+
+// @eventName     -> name
+// @battelVictor  -> victor
+// @description   -> description
+
+// Sortieren Sie das Ergebnis nach dem Eventnamen aufsteigend.
+// Übernehmen Sie nur die ersten 2 Dokumente.
+
+
+// Hinweis: Prüfung ob das review Array Elemente hat:
+
+//  reviews : {
+//       $exists : true, $not : { $size : 0 }
+// }
+
+db.events.find({});
+
+db.events.aggregate([
+    {
+        $match : {
+            eventType : "BATTLE",
+        }
+    },
+    {
+        $project : {
+            eventName : "$name",
+            battleVictor : "$victor",
+            description : "$description"
+        }
+    },
+    {
+        $sort : {
+            eventName : 1
+        }
+    },
+    {
+        $limit : 2
+    },
+    {
+        $out : "eventsReport"
+    }
+])
+
+
+
+
+
+
+
+
+db.events.aggregate([
+    {
+        $match : {
+            eventType : "BATTLE",
+            reviews : {
+                $exists : true, $not : { $size : 0 }
+            }
+        }
+    },
+    {
+        $project : {
+            eventName : "$name",
+            battelVictor : "$victor",
+            description : "$description"
+        }
+    },{
+      $limit : 2
+   },
+    {
+      $out : "eventsReport"
+   }
+]);
+
+// -- --------------------------------------------------------------------------------- --
+// --  2.Beispiel) Aggregationsframework - $group, $out                     (1.Punkt)
+// -- --------------------------------------------------------------------------------- --
+// Geben Sie für jeden eventType folgende Werte aus. Speichern Sie Ihr Ergebnis in einer
+// Collection eventReport
+
+// {
+//    "_id" : "BATTLE",
+//    "documentCount" : ..., @Anzahl der Dokumente für den eventType
+//    "eventNames" : [
+//        {
+//            "name" : "Battle of ...",
+//            "victor" : "..."
+//        }, ...
+//     ]
+//  }
+
+db.events.find({});
+
+db.events.aggregate([
+    {
+        $group : {
+            _id : "$eventType",
+            documentCount : {
+                $sum : 1
+            },
+            eventNames : {
+                $addToSet : {
+                    name : "$name",
+                    victor : "$victor"
+                }
+            }
+        }
+    },
+    {
+        $out : "eventsReport"
+    }
+]);
+
+
+
+// -- --------------------------------------------------------------------------------- --
+// --  3.Beispiel) Aggregationsframework - $match, $unwind, $addFields, $group, $lookup
+// --                                    - $project, $out
+// --  (3.Punkte)
+// -- --------------------------------------------------------------------------------- --
+
+// Sammeln Sie für events die folgenden Daten in der eventReport collection. Berück-
+// sichtigen Sie nur BATTLE eventTypes.
+//
+// eventType, description, territorialChange, belligerents
+//
+// @name        -> name
+// @eventType   -> eventType
+// @description -> description
+// @territorialChange -> territorialChange
+// @belligerents : [{
+//     commander : "...",
+//     nation : "...",
+//     troopCount : -> $sum composition.amount
+//     troopTypes : ["...", "..."]
+//     lossCount  : -> $sum losses.amount
+// }]
+
+
+// Hinweis: Zur Berechung der Werte der gegnerischen Seiten (belligerents)
+//          sollte eine "$unwind" auf dem Array durchgeführt werden.
+
+db.events.find({});
+
+db.events.aggregate([
+    {
+        $match : {
+            eventType : "BATTLE"
+        }
+    }, {
+        $addFields : {
+            newBelligerents : {
+                $map : {
+                    input : "$belligerents",
+                    as : "belligerents",
+                    in : {
+                        commander : "$$belligerents.commander",
+                        nation : "$$belligerents.nation",
+                        troopCount : {
+                            $sum : "$$belligerents.composition.amount"
+                        },
+                        troopTypes : "$$belligerents.composition.type",
+                        lossCount : {
+                            $sum : "$$belligerents.losses.amount"
+                        }
+                    }
+                }
+            }
+        }
+    }
+]);
+
+db.events.aggregate([
+
+]);
---
+++ dql/history/aggregate exercise 2.mongodb.js
@@ -0,0 +1,215 @@
+use ('insy');
+
+// -- ----------------------------------------------------------------- --
+// --  1.) Beispiel
+// -- ----------------------------------------------------------------- --
+// Für die Teilnehmer (Belligerents) an Schlachten soll ein Report ver-
+// faßt werden.
+
+// I) Geben Sie für alle Belligerents die folgenden Werte aus:
+
+//    var belligerent = {
+//         commander : "...",
+//         nation : "...",    
+//         battles : [  // Kurzbeschreibung einer Schlacht
+//             { name : "...", troopCount : ..., losses : ... },
+//               // troopCount und losses beziehen sich auf die
+//               // eigenen Truppen
+//             { ... }
+//         ],
+//         victoryCount : ...  // Anzahl der Siege des Generals
+//    }
+
+
+// II) Speichern Sie das Ergebnis in einer Collection: commander
+
+// Hinweis: Wenn nach mehreren Werten gruppiert werden soll, wird für
+//          die $group Stufe die folgende Syntax verwendet:
+
+// $group : {_id : { field1 : value1, field2: value2 } }
+
+
+db.events.find({});
+
+db.events.aggregate([
+{
+    $match : {
+        eventType : "BATTLE"
+    }
+}, {
+    $unwind : "$belligerents"
+}, {
+    $project : {
+        commander : "$belligerents.commander",
+        nation : "$belligerents.nation",
+        name : 1,
+        battle : {
+            name : "$name",
+            troopCount : {
+                $sum : "$belligerents.composition.amount"
+            },
+            losses : {
+                $sum : "$belligerents.losses.amount"
+            }
+        }
+    }
+},{
+    $group : {
+        _id: {commander : "$commander", nation : "$nation"},
+        battles : {
+            $addToSet : "$battle"
+        }
+    }
+},{
+     $lookup:
+     {
+       from: "events",
+       localField: "_id.commander",
+       foreignField: "victor",
+       as: "temp"
+     }
+}, {
+    $project : {
+        commander : "$_id.commander",
+        nation : "$_id.nation",
+        battles : 1,
+        victorCount : {
+            $size : "$temp"
+        }
+    }
+},{
+    $out : "commander"
+}]);
+
+
+// -- ----------------------------------------------------------------- --
+// --  2.) Beispiel
+// -- ----------------------------------------------------------------- --
+// Für Commander (collection commander) soll ein Report verfaßt werden.
+// Einem Commander werden dabei für bestimmte Aspekte Punkte zugeordnet.
+
+// I) Berechnen Sie für jeden Commander einen influenceCount. Der Wert
+//    für influenceCount setzt sich dabei aus folgenden Posten zusammen:
+
+//    * victories -> $victoryCount * 50
+//    * battleParticipation -> $(battle count) * 2
+//    * troopCount -> ($(overall Troopcount) / $(battle count))/1000
+
+// II) Geben Sie für jeden Commander die folgenden Felder aus. Speichern
+//     Sie Ihr Ergebnis in einer collection commanderReport.
+
+//     var commander = {
+//          name : "",
+//          nation : "",
+//          influenceCount : ...
+//     }
+
+db.commander.find({});
+
+// -- ----------------------------------------------------------------- --
+// --  1.) Beispiel
+// -- ----------------------------------------------------------------- --
+// Für die Teilnehmer (Belligerents) an Schlachten soll ein Report ver-
+// faßt werden.
+
+// I) Geben Sie für alle Belligerents die folgenden Werte aus:
+
+//    var belligerent = {
+//         commander : "...",
+//         nation : "...",    
+//         battles : [  // Kurzbeschreibung einer Schlacht
+//             { name : "...", troopCount : ..., losses : ... },
+//               // troopCount und losses beziehen sich auf die
+//               // eigenen Truppen
+//             { ... }
+//         ],
+//         victoryCount : ...  // Anzahl der Siege des Generals
+//    }
+
+
+// II) Speichern Sie das Ergebnis in einer Collection: commander
+
+// Hinweis: Wenn nach mehreren Werten gruppiert werden soll, wird für
+//          die $group Stufe die folgende Syntax verwendet:
+
+// $group : {_id : { field1 : value1, field2: value2 } }
+
+
+db.events.find({});
+
+db.events.aggregate([
+{
+    $match : {
+        eventType : "BATTLE"
+    }
+}, {
+    $unwind : "$belligerents"
+}, {
+    $project : {
+        commander : "$belligerents.commander",
+        nation : "$belligerents.nation",
+        name : 1,
+        battle : {
+            name : "$name",
+            troopCount : {
+                $sum : "$belligerents.composition.amount"
+            },
+            losses : {
+                $sum : "$belligerents.losses.amount"
+            }
+        }
+    }
+},{
+    $group : {
+        _id: {commander : "$commander", nation : "$nation"},
+        battles : {
+            $addToSet : "$battle"
+        }
+    }
+},{
+     $lookup:
+     {
+       from: "events",
+       localField: "_id.commander",
+       foreignField: "victor",
+       as: "temp"
+     }
+}, {
+    $project : {
+        commander : "$_id.commander",
+        nation : "$_id.nation",
+        battles : 1,
+        victorCount : {
+            $size : "$temp"
+        }
+    }
+},{
+    $out : "commander"
+}]);
+
+
+// -- ----------------------------------------------------------------- --
+// --  2.) Beispiel
+// -- ----------------------------------------------------------------- --
+// Für Commander (collection commander) soll ein Report verfaßt werden.
+// Einem Commander werden dabei für bestimmte Aspekte Punkte zugeordnet.
+
+// I) Berechnen Sie für jeden Commander einen influenceCount. Der Wert
+//    für influenceCount setzt sich dabei aus folgenden Posten zusammen:
+
+//    * victories -> $victoryCount * 50
+//    * battleParticipation -> $(battle count) * 2
+//    * troopCount -> ($(overall Troopcount) / $(battle count))/1000
+
+// II) Geben Sie für jeden Commander die folgenden Felder aus. Speichern
+//     Sie Ihr Ergebnis in einer collection commanderReport.
+
+//     var commander = {
+//          name : "",
+//          nation : "",
+//          influenceCount : ...
+//     }
+
+db.commander.find({});
+
+
