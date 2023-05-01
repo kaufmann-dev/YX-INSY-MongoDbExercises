d3159c6b7366addd4bebdf9fdcc47217b466c6a4
David Kaufmann <david@kaufmann.dev>
2023-05-01 22:42:34 +0200

    Update queries.mongodb.js

--- dql/outerrim/queries.mongodb.js
+++ dql/outerrim/queries.mongodb.js
@@ -1,4 +1,37 @@
 // gibt für jeden Charakter die Reputation pro Faction aus
+db.outerrim.find({});
+
+db.outerrim.aggregate([
+  {
+    $unwind: '$crew'
+  },
+  {
+    $unwind: '$factionReputation'
+  },
+  {
+    $group: {
+      _id: {
+        name : "$crew.name",
+        faction : "$factionReputation.faction"
+      },
+      factionReputations : {
+        $sum : "$factionReputation.change"
+      }
+    }
+  },
+  {
+    $group: {
+      _id: "$_id.name",
+      reputations: {
+        $addToSet : {
+          name : "$_id.faction",
+          reputation : "$factionReputations"
+        }
+        
+      }
+    }
+  }
+]);
 
 db.outerrim.aggregate([
     {
@@ -46,3 +79,97 @@ db.outerrim.aggregate([
         }
     }
 ]);
+
+//* gibt für alle Factions alle Charaktäre und die Reputation die sie bei ihr haben aus
+// "_id": "SYNDIKATE",
+// "characters": [
+//   {
+//     "character": "Chewbacca",
+//     "change": -130
+//   },
+//   {
+//     "character": "Han Solo",
+//     "change": -130
+//   }
+// ]
+
+db.outerrim.find({});
+
+db.outerrim.aggregate([
+  {
+    $unwind : "$factionReputation"
+  },
+  {
+    $unwind : "$crew"
+  },
+  {
+    $group: {
+      _id: {
+        name : "$factionReputation.faction"
+      },
+      changes: {
+        $addToSet : {
+          character: "$crew.name",
+          change: "$factionReputation.change"
+        }
+      }
+    }
+  },
+  {
+    $unwind : "$changes"
+  },
+  {
+    $
+  }
+]);
+
+db.outerrim.aggregate([{
+  $unwind : "$crew"
+}, {
+  $unwind : "$factionReputation"
+}, {
+  $group : {
+      _id : {
+          name : "$crew.name",
+          faction : "$factionReputation.faction"
+      },
+      change : {
+          $sum : "$factionReputation.change"
+      }
+  }
+}, {
+  $group : {
+      _id : "$_id.faction",
+      data : {
+          $push : "$$ROOT"
+      },
+      maxChange : {
+          $max : "$change"
+      }
+  }
+}, {
+  $addFields : {
+      data : {
+          $map : {
+              input : "$data",
+              as: "dataval",
+              in : {
+                  character : "$$dataval._id.name",
+                  change : "$$dataval.change"
+              }
+          }
+      }
+  }
+}, {
+  $project : {
+      characters : {
+          $filter : {
+              input : "$data",
+              as : "dataval",
+              cond : {
+                  $eq : ["$$dataval.change", "$maxChange"]
+              }
+          }
+      }
+  }
+}]);
